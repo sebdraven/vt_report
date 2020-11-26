865dc2e85ef6f81580509685fdb7fc0ccb090bd7
Sebdraven <sebdraven@protonmail.com>
2020-11-26 16:37:07 +0100

    Initial commit

---
+++ vt_taskc.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+from celery import Celery
+import requests
+from redis import StrictRedis
+
+celery_broker = 'redis://127.0.0.1:6379/5'
+celery_backend = 'redis://127.0.0.1:6379/5'
+
+celery = Celery('tasks', broker=celery_broker, backend= celery_backend)
+redis_client = StrictRedis()
+@celery.task
+
+def push(name):
+
+    redis_client.rpush('files', name)
+
+
+if redis_client.llen('files') % 10000 == 0:
+    print('number file to record %s' % redis_client.llen('files'))
+
+@celery.task
+def vt_report(hash_file, api_key):
+    params = {'resource': hash_file,
+              'apikey': api_key}
+    response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',
+                            params=params)
+
+    if response.status_code == 200:
+        r = response.json()
+        directory = 'jsons/%s/%s/%s/%s/%s' % (hash_file[0:2], hash_file[3:5], hash_file[6:8], hash_file[9:11], hash_file[12:14])
+        os.makedirs(directory, exist_ok=True)
+        json.dump(r, open(os.path.join(directory, '%s.json' % hash), 'w'))
+        print('records %s.json' % hash)
---
+++ vt_utils.py
@@ -0,0 +1,50 @@
+import argparse
+import glob
+import json
+import os
+import sys
+import time
+
+from redis import StrictRedis
+
+from vt_taskc import vt_report, push
+
+redis_client = StrictRedis()
+
+
+def all_files(path):
+    return [json.load(open(json_file))['sha256'] for json_file in glob.glob(path)
+             if os.path.isfile(json_file) and 'sha256' in json.load(open(json_file))]
+
+
+def vt_report_launcher(api_key):
+    while True:
+        h = redis_client.lpop('files')
+        vt_report.delay(h.decode(), api_key)
+        time.sleep(1)
+
+
+def record_file(malware_data='/data/malware_samples/DATASET'):
+
+    number_file = 0
+    for root, dirs, files in os.walk(malware_data):
+        for name in files:
+            push.delay(name)
+
+
+
+def parse_command_line():
+    parser = argparse.ArgumentParser(description='VT Labelling')
+    parser.add_argument('--record', dest='record', help='Command to record all files name in redis')
+    parser.add_argument('--vt_report', dest='vt_report', help='Launch report catcher of VT')
+    parser.add_argument('--label', dest='label', help='labelling vt report')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_command_line()
+    if args.record:
+        record_file()
+    if args.vt_report:
+        vt_report_launcher(args.vt_report)
